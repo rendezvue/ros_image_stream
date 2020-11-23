debf1eb2f8f4a79835dc8549367d753c6cc6a9cb
Juhong <juhong@rdv.ai>
2020-11-23 05:47:47 +0000

    rosrun 프로그램 확인

--- example/src/test.cpp
+++ example/src/test.cpp
@@ -1,8 +1,18 @@
 #include "opencv2/opencv.hpp"
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+	std::string str_server_name = "ensemble_nx_29" ;
+	
+	if( argc == 2)
+	{
+		str_server_name = argv[1] ;
+	}
 
+	std::string str_command = "rosrun ros_image_stream ros_image_stream _server_name:=" + str_server_name ;
+
+	system(str_command.c_str());
 	return 0 ;
 }
 
